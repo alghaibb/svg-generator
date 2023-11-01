75e257d61fb02fa6212e2a0e2960421c00648eb5
Moudi <mjaderi97@gmail.com>
2023-11-01 16:53:15 +1100

    Fixed square shape

--- generateSVG.js
+++ generateSVG.js
@@ -5,24 +5,24 @@
  * @returns {string} - The generated SVG string.
  */
 function generateSVG(details) {
-  let svgContent = '<svg width="200" height="200">';
+    let svgContent = '<svg width="200" height="200">';
 
-  switch (details.shape) {
-      case 'circle':
-          svgContent += `<circle cx="100" cy="100" r="50" fill="${details.color}" />`;
-          break;
-      case 'square':
-          svgContent += `<rect width="100" height="100" fill="${details.color}" />`;
-          break;
-      case 'triangle':
-          svgContent += `<polygon points="100,10 190,190 10,190" fill="${details.color}" />`;
-          break;
-  }
+    switch (details.shape) {
+        case 'circle':
+            svgContent += `<circle cx="100" cy="100" r="50" fill="${details.color}" />`;
+            break;
+        case 'square':
+            svgContent += `<rect x="50" y="50" width="100" height="100" fill="${details.color}" />`;
+            break;
+        case 'triangle':
+            svgContent += `<polygon points="100,10 190,190 10,190" fill="${details.color}" />`;
+            break;
+    }
 
-  svgContent += `<text x="100" y="100" fill="${details.textColor}" font-size="24" font-family="Arial" text-anchor="middle" dy=".3em">${details.name}</text>`;
-  svgContent += '</svg>';
+    svgContent += `<text x="100" y="100" fill="${details.textColor}" font-size="24" font-family="Arial" text-anchor="middle" dy=".3em">${details.name}</text>`;
+    svgContent += '</svg>';
 
-  return svgContent;
+    return svgContent;
 }
 
 module.exports = generateSVG;
